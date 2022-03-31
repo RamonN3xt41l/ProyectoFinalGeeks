938b78bd897cd89cfcffbbb34c655cb0b21918b7
Ramon <ramon@nextail.co>
2022-03-31 17:33:32 +0200

    refactoring + changing from array to dictionary

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-class mensajesdecanal {
-    constructor(nombreautor, horamensaje, contenidomensaje) {
-        this.nombreautor = 'UserRandom';
-        this.horamensaje = '2022-11-01 11:00';
-        this.contenidomensaje = contenidomensaje;
+class ChannelMessages {
+    constructor(authorname, messagetime, messagecontent) {
+        this.authorname = 'UserRandom';
+        this.messagetime = '2022-11-01 11:00';
+        this.messagecontent = 'contenidomensaje';
     }
 
     // aqui iran los metodos si los hubiere
 }
-var canales = []; // este es mi array de arrays
-var cadacanal = []; // este es mi array de cada canal, q tendra objetos de mensajesdecanal dentro
+//var channels = []; 
+let channels= new Map(); 
+var eachChannel = []; // este es mi array de cada canal, q tendra objetos de mensajesdecanal dentro
 /* var cadacanal0 = new Array(3) 
 cadacanal0[0] = 12 cadacanal0[1] = 10 cadacanal0[2] = 11 
 
@@ -24,56 +25,63 @@ canales[0] = cadacanal0 canales[1] = cadacanal1 canales[2] = cadacanal2
 */
 
 let text1 = document.getElementById("cajatextonuevocanal");
-let boton1 = document.getElementById("botonguardar");
-let myerror1 = document.getElementById("error1");
+let button1 = document.getElementById("botonguardar");
+let myError1 = document.getElementById("error1");
 
 
 function CreaCanal() 
 {
-    myerror1.innerHTML = '';
-    if(text1.hidden == true && boton1.hidden == true) 
+    myError1.innerHTML = '';
+    if(text1.hidden == true && button1.hidden == true) 
     {
         text1.hidden = false;
-        boton1.hidden = false
+        button1.hidden = false
     } else 
     {
         text1.hidden = true;
-        boton1.hidden = true;
+        button1.hidden = true;
     }
 }
 
 function GuardaCanal() 
 {
-    let objetoh = document.getElementById("mychannels");
+    let objetoH = document.getElementById("mychannels");
     let p =document.createElement("p");
-    var NombreCanal = document.getElementById("cajatextonuevocanal").value;
-    if(NombreCanal != '')
+    var channelName = document.getElementById("cajatextonuevocanal").value;
+    if(channelName != '')
     {
     //console.log(NombreCanal);
-        myerror1.innerHTML = ''
-        cadacanal.push(NombreCanal);
-        canales.push(cadacanal);
+        myError1.innerHTML = ''
+        eachChannel.push(channelName);
+        // channels.set(eachChannel);
     
-        for (let i = 0; i<canales.length; i++) 
+        for (let i = 0; i<eachChannel.length; i++) 
             {
+            channels.set(i,channelName)
             //p.innerHTML= canales[i];
-            console.log(canales[i]);
-            p.innerHTML= '<div class="mimenu" onclick="muestramensajes()">canales[i]</div>';
-            objetoh.appendChild(p);
+            //console.log(eachChannel[i]);
+            p.innerHTML= '<div class="mimenu" onclick="muestraMensajes('+i+')">'+eachChannel[i]+'</div>';
+            objetoH.appendChild(p);
             }
         document.getElementById("cajatextonuevocanal").value = "";
         text1.hidden = true;
-        boton1.hidden = true;
+        button1.hidden = true;
     } else 
     {
-        myerror1.innerHTML = 'Por favor introduce un valor'
+        myError1.innerHTML = 'Por favor introduce un valor'
 
     }
 }
 
-function muestramensajes() {
-    let contenedorprincipal = document.getElementById("contenedorprinc");
-    let mydiv =document.createElement("div");
-    mydiv.innerHTML = '<div class="mismensajes"> patatas sin pablo</div>';
-    contenedorprincipal.appendChild(mydiv);
+function muestraMensajes(channelKey) {
+    let cabeceradepagina = document.getElementById("cabeceradepagina");
+    let MainContainer = document.getElementById("contenedorprinc");
+    MainContainer.innerHTML = "";
+    let MyDiv =document.createElement("div");
+    var RamonMensaje = new ChannelMessages();
+    MyDiv.innerHTML = '<div class="mismensajes">'+RamonMensaje.messagecontent+'</div>';
+    MainContainer.appendChild(MyDiv);
 } 
+
+
+//hacer el primer array con un diccionario
