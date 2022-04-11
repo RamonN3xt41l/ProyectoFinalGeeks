a68566f4d2b7524f5ee5be210f00d609440558f8
Ramon <ramon@nextail.co>
2022-04-11 12:06:37 +0200

    more refactor

--- js/main.js
+++ js/main.js
@@ -13,38 +13,35 @@ let myError1 = document.getElementById("error1");
 let ChannelForm = document.getElementById("ChannelForm")
 let messageText = document.getElementById("NewMessageBox");
 let MessageButton = document.getElementById("SaveMessage");
-let MessageContainer = document.getElementById("MainFooter")
-
+let MessageContainer = document.getElementById("MainFooter");
+let objetoH = document.getElementById("mychannels");
+let MainContainer = document.getElementById("contenedorprinc");
+let PageHeader = document.getElementById("MainHeader");
 
 function CreateChannel() 
 {
     myError1.innerHTML = '';
-    if (window.getComputedStyle(ChannelForm).visibility === "hidden") {
+    if (window.getComputedStyle(ChannelForm).visibility === "hidden") 
+    {
         show(ChannelForm);
-    } else {
-        hide(ChannelForm);
-    }
+    } else hide(ChannelForm);
 }
 
 function SaveChannel() 
 {
-    let objetoH = document.getElementById("mychannels");
     let p =document.createElement("p");
     let channelName = document.getElementById("newchanneltextbox").value;
     if(channelName != ''){
         myError1.innerHTML = '';
         channels.set(channelName, new Array());
-        for (let key of channels.keys()) {
+        for (let key of channels.keys()) 
+        {
             p.innerHTML= '<div class="mimenu" onclick="ShowChannelMessages(\''+key+'\')">'+key+'</div>';
             objetoH.appendChild(p);
         }
         document.getElementById("newchanneltextbox").value = "";
         hide(ChannelForm);
-    } else 
-    {
-        myError1.innerHTML = 'Please enter the channel name'
-
-    }
+    } else myError1.innerHTML = 'Please enter the channel name';
 }
 
 function ShowChannelMessages(channelKey) 
@@ -52,28 +49,19 @@ function ShowChannelMessages(channelKey)
     if(window.getComputedStyle(MessageContainer).visibility === "hidden") {
         show(MessageContainer);
     }
-    let MainContainer = document.getElementById("contenedorprinc");
-    let PageHeader = document.getElementById("MainHeader")
     MainContainer.innerHTML = "";
     PageHeader.innerHTML="";
     PageHeader.innerHTML = `<div><div id="SearchingContainer">${channelKey}</div><input id="SearchMessageBox" type="text" oninput="SearchInChannel()" name="Search_message_box" placeholder="Search within this channel"></div>`;
     Messages = channels.get(channelKey); 
-    for (let i = 0; i< Messages.length; i++){
-        let MyDiv =document.createElement("div");
-        MyDiv.innerHTML = '<div class="ActualAuthorTime">'+Messages[i].AuthorName+'   '+Messages[i].MessageTime+'</div><div class="ActualMessages">'+Messages[i].MessageContent+'</div>';
-        MainContainer.appendChild(MyDiv);
-    }
+    DisplayMessages(Messages);
 } 
-
-
 
 function SaveMessage() 
 {
-    let MyDiv =document.createElement("div");
+    let MessageDiv =document.createElement("div");
     let ChanelForTheMessage = document.getElementById("SearchingContainer").innerHTML;
     let NewMessage = new ChannelMessages();
     let MessageWritten = document.getElementById("NewMessageBox").value;
-    let MainContainer = document.getElementById("contenedorprinc");
     NewMessage.MessageContent = MessageWritten;
     NewMessage.channelOwner= ChanelForTheMessage;
     NewMessage.AuthorName= 'RamonCampos';
@@ -81,10 +69,11 @@ function SaveMessage()
     Messages = channels.get(ChanelForTheMessage);
     Messages.push(NewMessage);
     document.getElementById("NewMessageBox").value = "";
-    MyDiv.innerHTML = "";
-    for (let i = 0; i< Messages.length; i++){
-        MyDiv.innerHTML = '<div class="ActualAuthorTime">'+Messages[i].AuthorName+' --> --> '+Messages[i].MessageTime+'</div><div class="ActualMessages">'+Messages[i].MessageContent+'</div>';
-        MainContainer.appendChild(MyDiv);
+    MessageDiv.innerHTML = "";
+    for (let i = 0; i< Messages.length; i++)
+    {
+        MessageDiv.innerHTML = '<div class="ActualAuthorTime">'+Messages[i].AuthorName+' --> --> '+Messages[i].MessageTime+'</div><div class="ActualMessages">'+Messages[i].MessageContent+'</div>';
+        MainContainer.appendChild(MessageDiv);
     }
 }
 
@@ -100,22 +89,21 @@ function show(ElementHtml){
 
 function SearchInChannel(){
 
-    let MessageSearched = document.getElementById("SearchMessageBox").value; // paso 1
-    let ChanelForTheMessage = document.getElementById("SearchingContainer").innerHTML; //recogiendo nombre del canal
-    let MainContainer = document.getElementById("contenedorprinc");
+    let MessageSearched = document.getElementById("SearchMessageBox").value; 
+    let ChanelForTheMessage = document.getElementById("SearchingContainer").innerHTML;
     let ExtraDiv = document.createElement("div");
     ChannelToSearch = channels.get(ChanelForTheMessage);
     MainContainer.innerHTML = "The messages you are looking for are:";
-    for (let i = 0; i< ChannelToSearch.length; i++){
-        if(ChannelToSearch[i].MessageContent.includes(MessageSearched)){
-            let MyDiv = document.createElement("div");
-            MyDiv.innerHTML = '<div class="SearchedDiv">'+ChannelToSearch[i].AuthorName+'   '+ChannelToSearch[i].MessageTime+'</div><div class="SearchedMessages">'+ChannelToSearch[i].MessageContent+'</div>';
-            MainContainer.appendChild(MyDiv);
+    for (let i = 0; i< ChannelToSearch.length; i++)
+    {
+        if(ChannelToSearch[i].MessageContent.includes(MessageSearched))
+        {
+            let SearchDiv = document.createElement("div");
+            SearchDiv.innerHTML = '<div class="SearchedDiv">'+ChannelToSearch[i].AuthorName+'   '+ChannelToSearch[i].MessageTime+'</div><div class="SearchedMessages">'+ChannelToSearch[i].MessageContent+'</div>';
+            MainContainer.appendChild(SearchDiv);
         } 
     }
-    if (MainContainer.childElementCount<1){
-            MainContainer.innerHTML = "There are no messages containing the text you are looking for"
-    }
+    if (MainContainer.childElementCount<1) MainContainer.innerHTML = "There are no messages containing the text you are looking for";
     ExtraDiv.innerHTML = 'In order to return to the channel messages please click here: <button onclick="ReturnToChannel()">Return</button>';
     MainContainer.appendChild(ExtraDiv);
 }
@@ -125,11 +113,24 @@ function ReturnToChannel() {
     let ChanelForTheMessage = document.getElementById("SearchingContainer").innerHTML;
     MainContainer.innerHTML = "";
     MessagesToShow = channels.get(ChanelForTheMessage);
+    DisplayMessages(MessagesToShow);
+    /*
     for (let i = 0; i< MessagesToShow.length; i++){
         let MyDiv =document.createElement("div");
         MyDiv.innerHTML = '<div class="ActualAuthorTime">'+Messages[i].AuthorName+' --> --> '+Messages[i].MessageTime+'</div><div class="ActualMessages">'+Messages[i].MessageContent+'</div>';
         MainContainer.appendChild(MyDiv);
     }
+    */
     document.getElementById("SearchMessageBox").value = "";
 
+}
+
+function DisplayMessages(ChannelToDIsplay) 
+{
+for (let i = 0; i< ChannelToDIsplay.length; i++)
+    {
+        let DisplayMessageDiv =document.createElement("div");
+        DisplayMessageDiv.innerHTML = '<div class="ActualAuthorTime">'+Messages[i].AuthorName+' --> --> '+Messages[i].MessageTime+'</div><div class="ActualMessages">'+Messages[i].MessageContent+'</div>';
+        MainContainer.appendChild(DisplayMessageDiv);
+    }
 }
